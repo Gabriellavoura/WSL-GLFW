2718e3150fbf763511b0ea3e82b6cae863c5c357
Gabriel <gabriellavoura@outlook.com>
2024-10-24 12:03:51 -0300

    Link glfw and added example

--- opengl/TestProject/src/main.cpp
+++ opengl/TestProject/src/main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
 
 using namespace std;
 
-int main() {
+int main(int, char**) {
 
-    cout << "Hello" << endl;
+    GLFWwindow* window;
 
+    if(!glfwInit()) {
+        return -1;
+    }
+
+    window = glfwCreateWindow(640, 480, "First window!", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
     return 0;
 }
