4a13edb87393676caa2c493db9e5f74a576ed7ac
Gabriel <gabriellavoura@outlook.com>
2024-10-27 22:53:45 -0300

    Improve cmake and fix libs path

--- opengl/TestProject/src/main.cpp
+++ opengl/TestProject/src/main.cpp
@@ -1,29 +1,41 @@
 #include <iostream>
+
+#include <glad/gl.h>
+
 #include <GLFW/glfw3.h>
 
-using namespace std;
+#include <GLFW/glfw3.h>
 
-int main(int, char**) {
-
+int main(void)
+{
     GLFWwindow* window;
 
-    if(!glfwInit()) {
+    /* Initialize the library */
+    if (!glfwInit())
         return -1;
-    }
 
-    window = glfwCreateWindow(640, 480, "First window!", NULL, NULL);
-    glfwMakeContextCurrent(window);
-
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        printf("Failed to initialize GLAD\n");
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
         glfwTerminate();
         return -1;
     }
 
-    while(!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
 
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
         glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
     }
 
     glfwTerminate();
